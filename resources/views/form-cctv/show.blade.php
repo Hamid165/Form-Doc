ca404f1f420def4b6112860b061f83f3470833d7
Hamid165 <hamid165@users.noreply.github.com>
2026-08-28 21:09:25 +0700

    update error

--- resources/views/form-cctv/show.blade.php
+++ resources/views/form-cctv/show.blade.php
@@ -336,7 +336,7 @@
 
             <div class="footer-section clearfix">
                 <div class="signature-box">
-                    <p>Yogyakarta, <span>{{ $form->kota_tanggal ? \Carbon\Carbon::parse($form->kota_tanggal)->locale('id')->translatedFormat('j F Y') : '................................' }}</span></p>
+                    <p>Yogyakarta, <span>{{ $form->kota_tanggal ?: '................................' }}</span></p>
                     <p style="margin-top: 15px;">Mengetahui,</p>
                     <p style="margin-top: 5px; text-align: center;">{{ $form->mengetahui_jabatan ?: '..........................................' }}</p>
                     <div style="height: 60px;"></div>
